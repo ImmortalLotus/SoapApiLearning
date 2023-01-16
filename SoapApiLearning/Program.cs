59b64aaafa7e8f6c4cc66e98f480a3896c9c0f2b
Joao Thomas de Souza Telles <joao.telles@setic.ro.gov.br>
2023-01-16 17:12:47 -0300

    Adicionar arquivos de projeto.

---
+++ SoapApiLearning/Program.cs
@@ -0,0 +1,7 @@
+﻿using static ServiceReference1.NumberConversionSoapTypeClient;
+
+ServiceReference1.NumberConversionSoapTypeClient client = new ServiceReference1.NumberConversionSoapTypeClient(EndpointConfiguration.NumberConversionSoap);
+
+string dolares=client.NumberToDollars(10);
+
+Console.WriteLine(dolares);
